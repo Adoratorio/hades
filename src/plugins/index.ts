e265d1091689016dfb8594245160b46e26c341b3
Daniele <daniele@adoratorio.com>
2024-11-05 17:18:55 +0100

    Added plugins export

---
+++ src/plugins/index.ts
@@ -0,0 +1,21 @@
+import LenisRender from "./lenis-render";
+import NativeRender from "./native-render";
+import Scrollbars from "./scrollbars";
+import StartStop from "./start-stop";
+import VirtualRender from "./virtual-render";
+
+const plugins = {
+  LenisRender,
+  NativeRender,
+  Scrollbars,
+  StartStop,
+  VirtualRender
+};
+
+export { LenisRender };
+export { NativeRender };
+export { Scrollbars };
+export { StartStop };
+export { VirtualRender };
+
+export default plugins;
